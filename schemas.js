de33a4880484292de1e3a813affe176d10c6c231
synchor020 <trandatkmatt@gmail.com>
2023-04-07 22:13:32 +0700

    all

--- schemas.js
+++ schemas.js
@@ -1,24 +1,24 @@
 
 const BaseJoi = require("joi");
-const sanitizeHtml = require('sanitize-html');
+//const sanitizeHtml = require('sanitize-html');
 const extension = (joi) => ({
     type: 'string',
-    base: joi.string(),
-    messages: {
-        'string.escapeHTML': '{{#label}} must not include HTML!'
-    },
-    rules: {
-        escapeHTML: {
-            validate(value, helpers) {
-                const clean = sanitizeHtml(value, {
-                    allowedTags: [],
-                    allowedAttributes: {},
-                });
-                if (clean !== value) return helpers.error('string.escapeHTML', { value })
-                return clean;
-            }
-        }
-    }
+    // base: joi.string(),
+    // messages: {
+    //     'string.escapeHTML': '{{#label}} must not include HTML!'
+    // },
+    // rules: {
+    //     escapeHTML: {
+    //         validate(value, helpers) {
+    //             const clean = sanitizeHtml(value, {
+    //                 allowedTags: [],
+    //                 allowedAttributes: {},
+    //             });
+    //             if (clean !== value) return helpers.error('string.escapeHTML', { value })
+    //             return clean;
+    //         }
+    //     }
+    // }
 });
 
 
@@ -26,11 +26,11 @@ const Joi = BaseJoi.extend(extension);
 
 module.exports.placeSchema = Joi.object({
     place: Joi.object({
-        title: Joi.string().required().escapeHTML(),
+        title: Joi.string().required(),
         price: Joi.number().required().min(0),
         //image: Joi.string().required(),
-        location: Joi.string().required().escapeHTML(),
-        description: Joi.string().required().escapeHTML()
+        location: Joi.string().required(),
+        description: Joi.string().required()
         
     }).required(),
     deleteImages: Joi.array()
@@ -39,6 +39,6 @@ module.exports.placeSchema = Joi.object({
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required().escapeHTML()
+        body: Joi.string().required()
     }).required()
 })
